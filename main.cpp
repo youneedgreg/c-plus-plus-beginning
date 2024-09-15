054f855b9de18f26ca5a2ee05f6a8c135f03396e
youneedgreg <gregorytemwa1212@gmail.com>
2024-09-16 02:51:04 +0300

    area of a triangle

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
-int main(){
-    cout<< "hello world\n";
+
+int main() {
+    // Display your name
+    cout << "Greg\n";
+
+    // Declare variables for base and height of the triangle
+    float base, height;
+
+    // Ask user to input the base and height
+    cout << "Enter the base of the triangle: ";
+    cin >> base;
+    cout << "Enter the height of the triangle: ";
+    cin >> height;
+
+    // Calculate the area of the triangle
+    float area = 0.5 * base * height;
+
+    // Display the area
+    cout << "The area of the triangle is: " << area << endl;
+
     return 0;
 }
-//slok
