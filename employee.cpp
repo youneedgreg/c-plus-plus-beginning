4b2e7444c70953f28048ed243e66071874bdeaa4
youneedgreg <gregorytemwa1212@gmail.com>
2024-09-18 00:42:16 +0300

    employee

---
+++ employee.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class employee
+{
+private:
+string empName, empNumber;
+
+public:
+void empDisplay() 
+{
+cout<<"Emloyee Name=" << empName << "\n";
+}
+void empAdd() 
+{
+cout << "Enter a  Name";
+cin >> empName;
+}};
+int main()
+{
+employee emp1,emp2;
+
+emp1.empAdd();
+emp2.empAdd();
+
+emp1.empDisplay();
+emp2.empDisplay();
+
+return 0;
+} //end main()
