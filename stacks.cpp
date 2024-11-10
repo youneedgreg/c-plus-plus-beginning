e6d3f1d409696a5fd95133d9a4f03e84acfda997
youneedgreg <gregorytemwa1212@gmail.com>
2024-11-11 00:31:34 +0300

    stacksss

--- stacks.cpp
+++ stacks.cpp
@@ -1,45 +1,40 @@
 #include <iostream>
 using namespace std;
 
-class StackX
-{
-private:
-int myStack[10];
-int top=0; //top of stack
+class StackX{
+    private:
+        int myStack[10];
+        int top = 0;
+    
+    public:
+        void push(int j){
+            myStack[++top] = j;
+        }
+        double() {
+            return myStack[top--];
+        }
+        double() {
+            return myStack[top];
+        }
 
-public:
-void push(int j) //put item on top - insert
-{
-myStack[++top] = j; //increment top,
-} //insert item
-
-double pop() //take item from top - delete
-{
-return myStack[top--]; //access item,
-} //decrement top
-
-double peek() //peek at top of stack - display
-{
-return myStack[top];
 }
-};
-int main()
-{
 
-StackX theStack; //make new stack, size 10
-theStack.push(20); //push items onto stack
-theStack.push(40);
-theStack.push(60);
-theStack.push(80);
+int main() {
+    StackX theStack; // Create a new stack
+    theStack.push(20);
+    theStack.push(40);
+    theStack.push(60);
+    theStack.push(80);
 
+        int i = 4;
+    while (i > 0) {
+        double value = theStack.pop();
+        i--;
+        cout << value << " ";
+    }
 
-int i=4;
-while(i>0) //until it’s empty,
-{ //delete item from stack
-double value = theStack.pop();
-i--;
-cout << value << " "; //display it
-} //end while
-cout << endl;
-return 0;
-} //end main()
+        cout << endl;
+    return 0;
+}
+
+}
