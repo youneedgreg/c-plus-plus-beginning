578b3c7ee10f6bb454905f9537324b64ea14f890
youneedgreg <gregorytemwa1212@gmail.com>
2024-11-11 01:01:07 +0300

    stacks learned

--- stacks.cpp
+++ stacks.cpp
@@ -5,36 +5,35 @@ class StackX{
     private:
         int myStack[10];
         int top = 0;
-    
+        
     public:
         void push(int j){
             myStack[++top] = j;
         }
-        double() {
+        double pop(){
             return myStack[top--];
         }
-        double() {
+        double peek(){
             return myStack[top];
         }
+};
 
-}
-
-int main() {
-    StackX theStack; // Create a new stack
+int main(){
+    StackX theStack;
     theStack.push(20);
+    theStack.push(30);
     theStack.push(40);
+    theStack.push(50);
     theStack.push(60);
-    theStack.push(80);
-
-        int i = 4;
-    while (i > 0) {
+    
+    int i = 4;
+    while(i > 0) {
         double value = theStack.pop();
         i--;
-        cout << value << " ";
+        cout<<value<<", ";
     }
-
-        cout << endl;
+    
+    cout << endl;
     return 0;
-}
-
+    
 }
