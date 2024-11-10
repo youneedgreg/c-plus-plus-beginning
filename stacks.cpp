e9aea6b505ce30df797e53f698a90ff15396e650
youneedgreg <gregorytemwa1212@gmail.com>
2024-11-11 01:04:30 +0300

    matching model

--- stacks.cpp
+++ stacks.cpp
@@ -1,39 +1,66 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-class StackX{
-    private:
-        int myStack[10];
-        int top = 0;
-        
-    public:
-        void push(int j){
-            myStack[++top] = j;
-        }
-        double pop(){
-            return myStack[top--];
-        }
-        double peek(){
-            return myStack[top];
-        }
-};
-
-int main(){
-    StackX theStack;
-    theStack.push(20);
-    theStack.push(30);
-    theStack.push(40);
-    theStack.push(50);
-    theStack.push(60);
-    
-    int i = 4;
-    while(i > 0) {
-        double value = theStack.pop();
-        i--;
-        cout<<value<<", ";
-    }
-    
-    cout << endl;
-    return 0;
-    
+class StackX
+{
+private:
+char myStack[10]; 
+int top; //top of stack
+public:
+StackX()
+{top=0;
 }
+	
+void push(char j) //put item on top
+{
+myStack[++top] = j; //increment top,
+} 
+
+char pop() //take item from top
+{
+return myStack[top--]; //access item,
+} //decrement top
+
+char peek() //peek at top of stack
+{
+return myStack[top];
+}
+bool isEmpty() //true if stack is empty
+{ return (top == 0); }
+};
+int main()
+{
+string input;
+cout << "Enter string containing delimiters and no whitespace"<<endl;
+cin >> input; 
+if( input.length() == 1 ) //single item cannot be matched
+return 0;
+
+StackX theStack; //make stack
+
+for(int j=0; j<input.length(); j++) //get chars in turn
+{
+char ch = input[j]; //get char
+switch(ch)
+{
+case '<': //opening symbols
+case '[':
+case '(':
+theStack.push(ch); //push them
+break;
+case '>': //closing symbols
+case ']':
+case ')':
+if( !theStack.isEmpty() ) //if stack not empty,
+{
+char chx = theStack.pop(); //pop and check
+if( (ch=='>' && chx!='<') ||(ch==']' && chx!='[') ||(ch==')' && chx!='(') )
+{
+cout << "Mismatched delimeter: "<< ch << " at char" << j << endl;
+}}
+break;
+} //end switch
+} //end for
+
+return 0;} //end main()
